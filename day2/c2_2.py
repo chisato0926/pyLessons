989e85d8ba84b01ca05b6ac6dc59c6194ce47625
chisato <chisatokobo@gmail.com>
2021-12-24 13:55:23 +0900

    c2_11追加 リスト操作

--- day2/c2_2.py
+++ day2/c2_2.py
@@ -1,2 +1,16 @@
-members=['工藤','松田','浅木']
+members=['工藤','松田','浅木'] #リスト作成
+print(members) #リスト参照
+print(members[0]) #リストの要素参照
+print(len(members)) #要素数
+members.append('山田') #要素の追加
 print(members)
+members[3]='山崎' #要素の書き換え
+print(members)
+members.remove('山崎') #要素の削除
+print(members)
+del members[0] #index指定での削除
+print(members)
+
+nums=[10,20,30]
+print(sum(nums)) #合計
+print(sum(nums)/len(nums)) #平均
