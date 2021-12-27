9bc1869960757705e508b6db1776985da50aee8b
chisato <chisatokobo@gmail.com>
2021-12-27 10:46:26 +0900

    C2_13.java追加 JavaでHashMapの復習

---
+++ day3/C2_13.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class C2_13{
+	public static void main(String[] args){
+		Map<String,Integer> scores=new HashMap<>();
+		scores.put("network",60);
+		scores.put("database",80);
+		scores.put("security",50);
+		System.out.println(scores);
+	}
+}
