f9a781d0a79ef072efd02919cd7223577bbdcce5
chisato <chisatokobo@gmail.com>
2022-01-17 14:13:39 +0900

    c6_11,c6_ex2,c6_ex3追加 リストと文字列による書き換え時のidentity値の変化

---
+++ day10/c6_11.py
@@ -0,0 +1,12 @@
+names=list()
+print(f'変更前のlistのidentity:{id(names)}')
+names.append('松田')
+print(f'変更後のlistのidentity:{id(names)}')
+
+name='松田'
+print(f'変更前のstrのidentity:{id(name)}')
+name='スーパー'+name
+print(f'変更後のstrのidentity:{id(name)}')
+
+name2='松田'
+print(f'name2のidentity:{id(name2)}')
